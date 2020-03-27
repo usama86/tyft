115b1738893df8b1c5d70279d74ccf37421dcb5a
Ahsan Farooq <batch15.038@gmail.com>
2020-03-27 06:02:09 -0700

    ok

--- Routes/Routes.js
+++ Routes/Routes.js
@@ -1,25 +1,32 @@
 import React from 'react';
-import { createStackNavigator } from '@react-navigation/stack';
+import {createStackNavigator} from '@react-navigation/stack';
 import HomeScreen from './../Screens/Auth/Home';
 import SignUpSelection from './../Screens/Auth/SignUpSelection';
 import SignIn from './../Screens/Auth/SignIn';
 import SignUp from './../Screens/Auth/SignUp';
 import * as RouteName from './../Constants/RouteName';
 
-
 const Stack = createStackNavigator();
-const Routes=()=>{
-          return(          
-            <Stack.Navigator initialRouteName="Home" screenOptions={{
-                headerShown: false
-              }}>
-                <Stack.Screen name={RouteName.HOME} component={HomeScreen}  options={{ title: 'Overview' }}/>
-                <Stack.Screen name={RouteName.SIGNUPSELECTION} component={SignUpSelection} />
-                <Stack.Screen name={RouteName.SIGNIN} component={SignIn} />
-                <Stack.Screen name={RouteName.SIGNUP} component={SignUp} />
-            </Stack.Navigator>
-            
-          )
-}
+const Routes = () => {
+  return (
+    <Stack.Navigator
+      initialRouteName="Home"
+      screenOptions={{
+        headerShown: false,
+      }}>
+      <Stack.Screen
+        name={RouteName.HOME}
+        component={HomeScreen}
+        options={{title: 'Overview'}}
+      />
+      <Stack.Screen
+        name={RouteName.SIGNUPSELECTION}
+        component={SignUpSelection}
+      />
+      <Stack.Screen name={RouteName.SIGNIN} component={SignIn} />
+      <Stack.Screen name={RouteName.SIGNUP} component={SignUp} />
+    </Stack.Navigator>
+  );
+};
 
 export default Routes;
