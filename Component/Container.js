c0e802d0e86447c012693ab3d47c24bb11fd7f68
Ahsan Farooq <batch15.038@gmail.com>
2020-03-29 13:33:42 -0700

    ok

--- Component/Container.js
+++ Component/Container.js
@@ -1,13 +1,17 @@
 import PropTypes from 'prop-types';
 import React from 'react';
-import {StyleSheet, SafeAreaView, View} from 'react-native';
+import {StyleSheet, SafeAreaView, View, ScrollView} from 'react-native';
 import {Container, Content} from 'native-base';
 
 const tyftContainer = ({containerStyle, ContentStyle, ...props}) => {
   const {container, content} = styles;
   return (
     <SafeAreaView {...props} style={[container, containerStyle]}>
-      <View style={[content, ContentStyle]}>{props.children}</View>
+      <View style={{flex: 1}}>
+        <ScrollView>
+          <View style={[content, ContentStyle]}>{props.children}</View>
+        </ScrollView>
+      </View>
     </SafeAreaView>
   );
 };
